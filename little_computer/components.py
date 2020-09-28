bb47ffb80082963bd08b6f57ee61d0680921041a
Oliver Gaskell <olivergaskell700@gmail.com>
2020-09-28 17:03:32 +0100

    Basic register functionality

--- little_computer/components.py
+++ little_computer/components.py
@@ -1,2 +1,17 @@
 """This module will contain classes for the various components which make up the CPU."""
 
+
+class Register:
+    """A CPU register."""
+
+    def __init__(self):
+        """Initialise Register."""
+        self.value = 0
+
+    def get(self):
+        """Get value stored in register."""
+        return self.value
+
+    def set(self, value):
+        """Set value stored in register."""
+        self.value = value
