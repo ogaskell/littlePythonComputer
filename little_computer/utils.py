e7c7466970d845255e58fb2d4e3628671c67e1ca
Oliver Gaskell <olivergaskell700@gmail.com>
2020-09-28 17:03:41 +0100

    Added clamp function

--- little_computer/utils.py
+++ little_computer/utils.py
@@ -1,2 +1,15 @@
 """Various Utility functions and classes."""
 
+
+def clamp(x, max, min, error=True):
+    """Ensure a value is within bounds."""
+    if min <= x <= max:
+        return x
+    else:
+        if error:
+            raise OverflowError("Value "+str(x)+" is not in bounds "+str(min)+" <= x <= "+str(max))
+        else:
+            if x > max:
+                return max
+            else:
+                return min
